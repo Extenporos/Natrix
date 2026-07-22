eafc691ae2a6b43683495c64d07302ab235eeba8
VortexNN <yasvn@icloud.com>
2026-07-22 15:46:53 -0600

    Remove test.py
    i don't know how this was here

--- test.py
+++
@@ -1,7 +0,0 @@
-from lib.NaParse.parser import Parser
-class idk:
-    def __init__(self):
-        self.rtn=Parser.parse(text="idk", debug=True)
-        print(self.rtn)
-if __name__ == "__main__":
-    idk
